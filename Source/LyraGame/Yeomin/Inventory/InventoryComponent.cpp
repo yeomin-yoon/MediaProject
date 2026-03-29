72908d920af9c27a78efdce407f18af3b0445906
byam12 <a01024985115@gmail.com>
2026-03-29 16:05:46 +0900

    12

---
+++ Source/LyraGame/Yeomin/Inventory/InventoryComponent.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryComponent.h"
+
+
+UInventoryComponent::UInventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+}
+
+
+void UInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                        FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
